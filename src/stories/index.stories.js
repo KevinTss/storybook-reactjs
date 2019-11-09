9146988799ddf6211614dc99685434d9c7581510
Kevin <kevin.tassi@gmail.com>
2019-11-09 10:51:21 +0100

    Create first button component

--- src/stories/index.stories.js
+++ src/stories/index.stories.js
@@ -1,10 +1,6 @@
 import React from 'react';
-import { Button } from '@storybook/react/demo';
+import Button from '../components/button';
 
 export default { title: 'Button' };
 
-export const withText = () => <Button>Hello Button</Button>;
-
-export const withEmoji = () => (
-  <Button><span role="img" aria-label="so cool">😀 😎 👍 💯</span></Button>
-);
+export const withText = () => <Button label="Hello world"/>;
