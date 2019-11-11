5cfbf1231f6ce8e107b5e06b94b384c45435c916
Kevin <kevin.tassi@gmail.com>
2019-11-11 10:09:25 +0100

    Refactor stories using knobs addon

--- src/stories/index.stories.js
+++ src/stories/index.stories.js
@@ -1,14 +1,16 @@
 import React from 'react'
-import { withKnobs, text } from '@storybook/addon-knobs'
+import { withKnobs, text, select } from '@storybook/addon-knobs'
 
 import Button from '../components/button'
 
 export default {
-  title: 'UI/Button',
+  title: 'UI',
   decorators: [withKnobs],
 }
 
-export const Basic = () => <Button label={text('Label', 'Button')} />
-export const Primary = () => (
-  <Button label={text('Label', 'Button')} type='primary' />
+export const button = () => (
+  <Button
+    label={text('Label', 'Button')}
+    type={select('Type', ['default', 'primary'], 'default')}
+  />
 )
